9a48f74b45dce4f1220a4e525fb908a7acc83ec2
Mason Armstrong <masonarm@gmail.com>
2016-08-01 20:03:53 -0500

    Updates reference to handle, renames to stream.

--- player.py
+++ player.py
@@ -12,7 +12,7 @@ class status(object):
 		self.streaming=False
 		self.songindex=0
 		self.songs=[]
-		self.handle=0
+		self.stream=0
 		self.tempo=0
 
 filename=""
@@ -20,12 +20,12 @@ p=status()
 o=output.Output()
 
 def open_file(filen="",fn=""):
-	p.handle =stream.FileStream(file=filen)
+	p.stream =stream.FileStream(file=filen)
 	filename=fn
 	p.streaming=False
 	p.loaded=True
-#	p.tempo=tempo.Tempo(p.handle.handle)
-	p.handle.volume=config.appconfig['general']['volume']
+	p.tempo=tempo.Tempo(p.stream)
+	p.stream.volume=config.appconfig['general']['volume']
 
 def add_file(filen="",fn=""):
 	p.songs.append(filen)
@@ -47,48 +47,48 @@ def open_playlist(filename,path):
 	switchtrack(0)
 
 def open_stream(filen=""):
-	p.handle =stream.URLStream(url=filename)
+	p.stream =stream.URLStream(url=filename)
 	p.streaming=True
 	p.loaded=True
 	p.songindex=0
 	play()
 
 def play():
-	if not p.handle==0:
-		p.handle.play()
+	if not p.stream==0:
+		p.stream.play()
 
 def stop():
-	if not p.handle==0:
-		p.handle.stop()
-		p.handle.set_position(0)
+	if not p.stream==0:
+		p.stream.stop()
+		p.stream.set_position(0)
 def pause():
-	if not p.handle==0:
-		p.handle.pause()
+	if not p.stream==0:
+		p.stream.pause()
 
 def seek(step):
-	pos=p.handle.get_position()
-	pos=p.handle.bytes_to_seconds(pos)
+	pos=p.stream.get_position()
+	pos=p.stream.bytes_to_seconds(pos)
 	pos+=step
-	pos=p.handle.seconds_to_bytes(pos)
+	pos=p.stream.seconds_to_bytes(pos)
 	if pos<0:
 		pos=0
-	p.handle.set_position(pos)
+	p.stream.set_position(pos)
 
 def speak_remaining_time():
-	pos=p.handle.get_position()
-	pos2=p.handle.get_length()
-	pos=p.handle.bytes_to_seconds(pos)
-	pos2=p.handle.bytes_to_seconds(pos2)
+	pos=p.stream.get_position()
+	pos2=p.stream.get_length()
+	pos=p.stream.bytes_to_seconds(pos)
+	pos2=p.stream.bytes_to_seconds(pos2)
 	speak(timefuncs.grt((pos2*1000)-(pos*1000)))
 
 def speak_elapsed_time():
-	pos=p.handle.get_position()
-	pos=p.handle.bytes_to_seconds(pos)
+	pos=p.stream.get_position()
+	pos=p.stream.bytes_to_seconds(pos)
 	speak(timefuncs.grt(pos*1000))
 
 def speak_length():
-	pos=p.handle.get_length()
-	pos=p.handle.bytes_to_seconds(pos)
+	pos=p.stream.get_length()
+	pos=p.stream.bytes_to_seconds(pos)
 	speak(timefuncs.grt(pos*1000))
 
 def switchtrack(step):
@@ -97,7 +97,7 @@ def switchtrack(step):
 		p.songindex=0
 	s=p.songs[p.songindex].split(":")
 	try:
-		playing=p.handle.is_playing
+		playing=p.stream.is_playing
 	except:
 		playing=False
 
